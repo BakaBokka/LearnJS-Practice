77c9e66806642fea8b3c1bbf177caa68debcf0ae
Damir <damirvakhitov@gmail.com>
2023-11-23 19:58:23 +0300

    Begin Object Basics

---
+++ JSBasics/ObjectsBasics/4.1.Objects.js
@@ -0,0 +1,95 @@
+//Объекты
+
+// Привет, object
+// Напишите код, выполнив задание из каждого пункта отдельной строкой:
+
+// Создайте пустой объект user.
+// Добавьте свойство name со значением John.
+// Добавьте свойство surname со значением Smith.
+// Измените значение свойства name на Pete.
+// Удалите свойство name из объекта.
+
+const user = {};
+user.name = "Baka";
+user.surname = "Bokka";
+user.name = "Pete?"
+delete user.name;
+
+
+// 2) Проверка на пустоту
+// Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+
+// Должно работать так:
+
+// let schedule = {};
+
+// alert( isEmpty(schedule) ); // true
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) ); // false
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+
+console.log(isEmpty({ 1: "d" }))
+
+// 4) Сумма свойств объекта
+// У нас есть объект, в котором хранятся зарплаты нашей команды:
+
+
+// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum.
+// Должно получиться 390.
+
+// Если объект salaries пуст, то результат должен быть 0.
+
+let sum = 0;
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+for (let num in salaries) {
+  sum += salaries[num]
+}
+
+const sumReduced = Object.values(salaries).reduce((acc, curr) => acc + curr, 0)
+
+console.log(sum, sumReduced)
+
+// 5)Умножаем все числовые свойства на 2
+// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+// Обратите внимание, что multiplyNumeric не нужно ничего возвращать. Следует напрямую изменять объект.
+
+// P.S. Используйте typeof для проверки, что значение свойства числовое.
+
+// Например:
+// до вызова функции
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+
+// после вызова функции
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      obj[key] *= 2
+    }
+  }
+}
+multiplyNumeric(menu)
+
